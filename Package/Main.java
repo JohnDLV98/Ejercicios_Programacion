12aefc938834f619ecfe9da21ed447d281ce1ab3
JohnDLV98 <jdlondono100171@umanizales.edu.co>
2022-04-29 19:17:58 -0500

    Parcial

--- Package/Main.java
+++ Package/Main.java
@@ -1,8 +1,58 @@
 package Package;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+
+    Scanner entrada = new Scanner(System.in);    
+    System.out.println("Ingrese cantidad de numeros que desee: ");       
+          
+        int n = entrada.nextInt(); 
+    int[] arr = new int[n];
+
+    for(int i=0; i<n; i++){
+        System.out.println("Escriba numero para añadir: ");
+        int a = entrada.nextInt(); 
+        arr[i] = a;
     }
+        System.out.println();
+        System.out.print(" [");
+        for(int j=0;j<n; j++){
+            System.out.print(" '"+ arr[j] + "' ");
+        } 
+
+        System.out.println("] ");
+
+        int[] arrinv = new int[n];
+
+        arrinv= invertirArreglo(arr);
+
+        System.out.println();
+        System.out.print(" [");
+        for(int k=0;k<n; k++){
+            System.out.print(" '"+ arr[k] + "' ");
+        } 
+
+        System.out.println("] ");
+    }
+
+    public static int[]  invertirArreglo(int[] arreglo) {
+        int temporal; 
+        int longitudDeArreglo = arreglo.length;
+
+        for (int x = 0; x < longitudDeArreglo / 2; x++) {
+          
+          temporal = arreglo[x];
+          
+          int indiceContrario = longitudDeArreglo - x - 1;
+          
+          arreglo[x] = arreglo[indiceContrario];
+          
+          arreglo[indiceContrario] = temporal;
+        }
+        
+        return arreglo;
+      }
 }
